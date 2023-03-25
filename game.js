dcf11380186db37316571dc9339513b433286d34
austinsipp <austin.sipp@gmail.com>
2023-03-24 21:21:27 -0700

    started populating valid moves

--- game.js
+++ game.js
@@ -1,145 +1,109 @@
 let gameBoard = document.querySelector("#gameBoard")
 
-function squareIdMap (id) {
-    let row = 9 - id[0]
-    let columnTransform = ["A","B","C","D","E","F","G","H"]
-    let column = columnTransform[id[1]-1]
-    return column + row
-}
-
-function squareNameToCoords (squareName) {
-    let y = parseInt(squareName.substring(1,2))
-    let xLetter = squareName.substring(0,1)
-    let columnTransform = ["A","B","C","D","E","F","G","H"]
-    let x = columnTransform.indexOf(xLetter) + 1
-    return [x,y]
-}
-//console.log(squareNameToCoords ("A2"))
-
-function squareCoordsToName (squareCoords) {
-    let row = squareCoords[1]
-    let columnTransform = ["A","B","C","D","E","F","G","H"]
-    let column = columnTransform[squareCoords[0]-1]
-    return column + row
-}
-
-for (let i=1; i<=8; i++) {
-    let row = document.createElement("div")
-    row.style.display = "flex"
-    for (let j=1; j<=8; j++) {
-        let square = document.createElement("div")
-        square.style.height = "5.625vw"
-        square.style.width = "5.625vw"
-        square.style.outlineColor = "black"
-        square.style.outlineStyle = "solid"
-        square.style.outlineWidth = "1px"
-        square.style.padding = "0"
-        square.style.margin = "0"
-        square.style.outlineOffset = "-1px"
-        square.style.textAlign = "center"
-        square.id = squareIdMap([i,j])
-        row.append(square)
-        if (i%2 === 0 && j%2 === 0) {
-            square.style.backgroundColor = "white"
-        } else if (i%2 != 0 && j%2 === 0) {
-            square.style.backgroundColor = "#00000050"
-        } else if (i%2 === 0 && j%2 != 0) {
-            square.style.backgroundColor = "#00000050"
-        } else if (i%2 != 0 && j%2 != 0){
-            square.style.backgroundColor = "white"
-        }
-    }
-    gameBoard.append(row)
-}
-
 
 let whitePieces = [
     {
         icon: "./assets/whitePawn.png",
         initialSpot:"A2",
-        pieceType: "pawn"
+        pieceType: "pawn",
+        pieceId: "queensRookPawn"
     },
     {
         icon: "./assets/whitePawn.png",
         initialSpot:"B2",
-        pieceType: "pawn"
+        pieceType: "pawn",
+        pieceId: "queensKnightPawn"
     },
     {
         icon: "./assets/whitePawn.png",
         initialSpot:"C2",
-        pieceType: "pawn"
+        pieceType: "pawn",
+        pieceId: "queensBishopPawn"
     },
     {
         icon: "./assets/whitePawn.png",
         initialSpot:"D2",
-        pieceType: "pawn"
+        pieceType: "pawn",
+        pieceId: "queensPawn"
     },
     {
         icon: "./assets/whitePawn.png",
         initialSpot:"E2",
-        pieceType: "pawn"
+        pieceType: "pawn",
+        pieceId: "kingsPawn"
     },
     {
         icon: "./assets/whitePawn.png",
         initialSpot:"F2",
-        pieceType: "pawn"
+        pieceType: "pawn",
+        pieceId: "kingsBishopPawn"
     },
     {
         icon: "./assets/whitePawn.png",
         initialSpot:"G2",
-        pieceType: "pawn"
+        pieceType: "pawn",
+        pieceId: "kingsKnightPawn"
     },
     {
         icon: "./assets/whitePawn.png",
         initialSpot:"H2",
-        pieceType: "pawn"
+        pieceType: "pawn",
+        pieceId: "kingsRookPawn"
     },
     {
         icon: "./assets/whiteRook.png",
         initialSpot:"A1",
-        pieceType: "rook"
+        pieceType: "rook",
+        pieceId: "queensRook"
     }
     ,
     {
         icon: "./assets/whiteKnight.png",
         initialSpot:"B1",
-        pieceType: "knight"
+        pieceType: "knight",
+        pieceId: "queensKnight"
     }
     ,
     {
         icon: "./assets/whiteBishop.png",
         initialSpot:"C1",
-        pieceType: "bishop"
+        pieceType: "bishop",
+        pieceId: "queensBishop"
     }
     ,
     {
         icon: "./assets/whiteQueen.png",
         initialSpot:"D1",
-        pieceType: "queen"
+        pieceType: "queen",
+        pieceId: "queen"
     }
     ,
     {
         icon: "./assets/whiteKing.png",
         initialSpot:"E1",
-        pieceType: "king"
+        pieceType: "king",
+        pieceId: "king"
     }
     ,
     {
         icon: "./assets/whiteBishop.png",
         initialSpot:"F1",
-        pieceType: "bishop"
+        pieceType: "bishop",
+        pieceId: "kingsBishop"
     }
     ,
     {
         icon: "./assets/whiteKnight.png",
         initialSpot:"G1",
-        pieceType: "knight"
+        pieceType: "knight",
+        pieceId: "kingsKnight"
     }
     ,
     {
         icon: "./assets/whiteRook.png",
         initialSpot:"H1",
-        pieceType: "rook"
+        pieceType: "rook",
+        pieceId: "kingsRook"
     }
 ]
 
@@ -234,26 +198,74 @@ let blackPieces = [
     }
 ]
 
+
+
+function squareIdMap (id) {
+    let row = 9 - id[0]
+    let columnTransform = ["A","B","C","D","E","F","G","H"]
+    let column = columnTransform[id[1]-1]
+    return column + row
+}
+
+function squareNameToCoords (squareName) {
+    let y = parseInt(squareName.substring(1,2))
+    let xLetter = squareName.substring(0,1)
+    let columnTransform = ["A","B","C","D","E","F","G","H"]
+    let x = columnTransform.indexOf(xLetter) + 1
+    return [x,y]
+}
+//console.log(squareNameToCoords ("A2"))
+
+function squareCoordsToName (squareCoords) {
+    let row = squareCoords[1]
+    let columnTransform = ["A","B","C","D","E","F","G","H"]
+    let column = columnTransform[squareCoords[0]-1]
+    let returnValue
+    if ( squareCoords[0] >= 1 && squareCoords[0] <= 8 && squareCoords[1] >= 1 && squareCoords[1] <= 8 ) {
+        returnValue = column + row
+    } else {
+        returnValue = "invalid"
+    }
+    return returnValue
+}
+
+for (let i=1; i<=8; i++) {
+    let row = document.createElement("div")
+    row.style.display = "flex"
+    for (let j=1; j<=8; j++) {
+        let square = document.createElement("div")
+        square.style.height = "5.625vw"
+        square.style.width = "5.625vw"
+        square.style.outlineColor = "black"
+        square.style.outlineStyle = "solid"
+        square.style.outlineWidth = "1px"
+        square.style.padding = "0"
+        square.style.margin = "0"
+        square.style.outlineOffset = "-1px"
+        square.style.textAlign = "center"
+        square.id = squareIdMap([i,j])
+        row.append(square)
+        if (i%2 === 0 && j%2 === 0) {
+            square.style.backgroundColor = "white"
+        } else if (i%2 != 0 && j%2 === 0) {
+            square.style.backgroundColor = "#00000050"
+        } else if (i%2 === 0 && j%2 != 0) {
+            square.style.backgroundColor = "#00000050"
+        } else if (i%2 != 0 && j%2 != 0){
+            square.style.backgroundColor = "white"
+        }
+    }
+    gameBoard.append(row)
+}
+
+
+
+
 let allPieces = {white: whitePieces, black: blackPieces}
-//console.log(allPieces.white)
+
 let gameData = {turn: "white", boardPosition: allPieces}
 
 let squaresObject = []
-
-/*
-for (let column=1; column<=8; column++) {
-    squaresObject.push([])
-    for (let row=1; row<=8; row++) {
-        squaresObject[column-1].push([])
-        squaresObject[column-1][row-1].push(
-            {
-                squareName: squareCoordsToName([row,column]),
-                squareCoords: [row,column],
-                occupied: "unoccupied"
-            })
-    }
-}
-console.log(squaresObject)*/
 
 
 function mapPiecesToSquaresObject() {
@@ -266,12 +278,15 @@ function mapPiecesToSquaresObject() {
 }
 
 
+
+
 function resetBoard () {
     function resetPieces (piece) {
         let pieceObject = document.createElement("img")
         pieceObject.src = piece.icon
         pieceObject.style.height = "5.625vw"
         pieceObject.style.width = "5.625vw"
+        pieceObject.id = `${piece.pieceId}`
         let location = document.querySelector(`#${piece.initialSpot}`)
         location.append(pieceObject)
         piece.currentSpot = piece.initialSpot
@@ -303,12 +318,139 @@ function resetBoard () {
     mapPiecesToSquaresObject()
 }
 
-resetBoard()
-console.log(squaresObject)
 
 function checkValidMoves(piece) {
+    let validMovesArray = []
+    switch(piece.pieceType) {
+        case ("pawn"):
+            let validMoveCounter = 0
+            //capturing
+            if (piece.color === "white") {
+                let newCoords = [piece.currentCoord[0] - 1, piece.currentCoord[1] + 1]
+                if (newCoords[0] >= 1 && newCoords[0] <=8 && newCoords[1] >= 1 && newCoords[1] <=8 &&
+                    squaresObject[newCoords[0]-1][newCoords[1]-1][0].occupied === "black") {
+                    validMovesArray.push(squareCoordsToName(newCoords))
+                    validMoveCounter++
+                }
+                newCoords = [piece.currentCoord[0] + 1, piece.currentCoord[1] + 1]
+                if (newCoords[0] >= 1 && newCoords[0] <=8 && newCoords[1] >= 1 && newCoords[1] <=8 &&
+                    squaresObject[newCoords[0]-1][newCoords[1]-1][0].occupied === "black") {
+                    validMovesArray.push(squareCoordsToName(newCoords))
+                    validMoveCounter++
+                }
+            }
+            if (piece.color === "black") {
+                let newCoords = [piece.currentCoord[0] - 1, piece.currentCoord[1] - 1]
+                if (newCoords[0] >= 1 && newCoords[0] <=8 && newCoords[1] >= 1 && newCoords[1] <=8 &&
+                    squaresObject[newCoords[0]-1][newCoords[1]-1][0].occupied === "white") {
+                    validMovesArray.push(squareCoordsToName(newCoords))
+                    validMoveCounter++
+                }
+                newCoords = [piece.currentCoord[0] + 1, piece.currentCoord[1] - 1]
+                if (newCoords[0] >= 1 && newCoords[0] <=8 && newCoords[1] >= 1 && newCoords[1] <=8 &&
+                    squaresObject[newCoords[0]-1][newCoords[1]-1][0].occupied === "white") {
+                    validMovesArray.push(squareCoordsToName(newCoords))
+                    validMoveCounter++
+                }
+            }
+            //first move option to move one or two
+            if (piece.moveCount === 0) {
+                for(let squaresMoved = 1; squaresMoved <= 2; squaresMoved++) {
+                    let newCoords = []
+                    if (piece.color === "white") {
+                        newCoords = [piece.currentCoord[0], piece.currentCoord[1] + squaresMoved]
+                    } else if (piece.color === "black") {
+                        newCoords = [piece.currentCoord[0], piece.currentCoord[1] - squaresMoved]
+                    }
+                    if (newCoords[0] >= 1 && newCoords[0] <=8 && newCoords[1] >= 1 && newCoords[1] <=8 &&
+                        squaresObject[newCoords[0]-1][newCoords[1]-1][0].occupied === "unoccupied") {
+                        validMovesArray.push(squareCoordsToName(newCoords))
+                        validMoveCounter++
+                    }
+                }
+            }
+            //subsequent moves only 1 forward
+            if (piece.moveCount != 0) {
+                let newCoords = []
+                if (piece.color === "white") {
+                    newCoords = [piece.currentCoord[0], piece.currentCoord[1] + squaresMoved]
+                } else if (piece.color === "black") {
+                    newCoords = [piece.currentCoord[0], piece.currentCoord[1] - squaresMoved]
+                }
+                if (newCoords[0] >= 1 && newCoords[0] <=8 && newCoords[1] >= 1 && newCoords[1] <=8 &&
+                    squaresObject[newCoords[0]-1][newCoords[1]-1][0].occupied === "unoccupied") {
+                    validMovesArray.push(squareCoordsToName(newCoords))
+                    validMoveCounter++
+                }
+            }
+        break
+        case ("knight"):
 
+        break
+        case ("bishop"):
+
+        break
+        case ("rook"):
+
+        break
+        case ("queen"):
+
+        break
+        case ("king"):
+
+        break
+    }
+    return validMovesArray
 }
 
+
+function populateValidMoves (gameData) {
+    if (gameData.turn === "white") {
+        gameData.boardPosition.white.forEach((piece) => {
+            let validMovesArray = checkValidMoves(piece)
+            let pieceObject = document.querySelector(`#${piece.pieceId}`)
+            pieceObject.addEventListener('click', () => {
+                document.querySelector(`#${piece.currentSpot}`).style.backgroundColor = "green"
+            })
+            //validMovesArray.forEach()
+            //let validSquare = document.querySelector.
+        }
+        )
+    } 
+    else if (gameData.turn === "black") {
+        gameData.boardPosition.black.forEach((piece) => {
+            let pieceObject = document.querySelector(`#${piece.pieceId}`)
+            pieceObject.addEventListener('click', () => {
+                let validMovesArray = checkValidMoves(piece)
+                console.log("did I make it?")
+                console.log(validMovesArray)
+                document.querySelector(`#${piece.currentSpot}`).style.backgroundColor = "green"
+                console.log("made it here")
+                validMovesArray.forEach((validMoveSquare) => {
+                    console.log($validMoveSquare)
+                    document.querySelector(`#${validMoveSquare}`).style.backgroundColor = "yellow"
+                })
+            })
+            //validMovesArray.forEach()
+            //let validSquare = document.querySelector.
+        }
+        )
+    }
+}
+
+
+
+
+
+
+
+resetBoard()
+//console.log(squaresObject)
+//console.log(whitePieces)
+
+//console.log(whitePieces[2])
+//console.log(checkValidMoves(whitePieces[2]))
+
+populateValidMoves(gameData)
 
 
